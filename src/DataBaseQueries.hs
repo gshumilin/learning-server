9703480fd88565f04233be8f32452fc657bccf42
gshumilin <gshumilin1234@gmail.com>
2022-05-27 21:21:43 +0700

    changed the return value of the writeUser function

--- src/DataBaseQueries.hs
+++ src/DataBaseQueries.hs
@@ -2,7 +2,6 @@ module DataBaseQueries where
 
 import Types.User
 import Database.PostgreSQL.Simple
-import Data.Int (Int64)
 
 parseUsersList :: IO [(User)]
 parseUsersList = do
@@ -10,9 +9,9 @@ parseUsersList = do
     res <- query_ conn "SELECT name,login,password,createDate,isAdmin,isAbleToCreateNews FROM userslist"
     return res
     
-writeUser :: User -> IO Int64
+writeUser :: User -> IO ()
 writeUser User {..} = do
     conn <- connectPostgreSQL "host='localhost' port=5432 dbname='learning_server_db' user='learning_server_user' password='pleasedonthackme'"
-    res <- execute conn "insert into userslist (name,login,password,createDate,isAdmin,isAbleToCreateNews) values (?,?,?,?,?,?)"
+    execute conn "insert into userslist (name,login,password,createDate,isAdmin,isAbleToCreateNews) values (?,?,?,?,?,?)"
                     (name, login, password, createDate, isAdmin, isAbleToCreateNews)
-    return res
+    return ()
