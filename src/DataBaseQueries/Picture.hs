77541ba36bc326915b3aab802ce9523390f6df7b
gshumilin <gshumilin1234@gmail.com>
2022-07-08 20:55:26 +0700

    findPicturesArray refactoring

--- src/DataBaseQueries/Picture.hs
+++ src/DataBaseQueries/Picture.hs
@@ -4,8 +4,9 @@ import Types.Domain.Picture
 import Database.PostgreSQL.Simple
 
 findPicturesArray :: Connection -> Int -> IO (Maybe [Picture])
-findPicturesArray conn pictureID = do
-    res <- query conn "SELECT base64 FROM pictures WHERE id = ?" $ Only pictureID
+findPicturesArray conn newsID = do
+    let q = "SELECT p.base64 FROM news_pictures np LEFT JOIN pictures p on p.id=np.picture_id WHERE np.news_id = ?"
+    res <- query conn q $ Only newsID
     if not . null $ res
         then return $ Just res
         else return Nothing
