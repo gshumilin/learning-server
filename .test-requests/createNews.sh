92e030db95ea4a9fda8cd56f99c9c5944010457c
gshumilin <gshumilin1234@gmail.com>
2022-09-12 16:10:03 +0700

    correct tests requests

--- .test-requests/createNews.sh
+++ .test-requests/createNews.sh
@@ -1,5 +1,5 @@
 curl -H "Content-type: application/json"\
-     -H "Authorization: YWRtaW46YWRtaW4="\
+     -H "Authorization: T2xlZ18xOnBhc3N3b3JkXzE="\
      -X POST\
      -d @createNews.json\
      localhost:3000/createNews
--- .test-requests/getNews.sh
+++ .test-requests/getNews.sh
@@ -1,4 +1,4 @@
 curl -H "Content-type: application/json"\
-     -H "Authorization: YWRtaW46YWRtaW4="\
+     -H "Authorization: T2xlZ18xOnBhc3N3b3JkXzE="\
      -X GET\
      localhost:3000/getNews
