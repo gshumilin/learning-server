b0bcdd5664f03ce0325a674688654bf7b1be78b6
gshumilin <gshumilin1234@gmail.com>
2022-10-08 11:54:00 +0700

    made draft of hCreateUset #15

--- src/Handlers/MainHandle.hs
+++ src/Handlers/MainHandle.hs
@@ -1,8 +1,9 @@
 module MainHandle where
 
+import Data.Maybe (isJust)
 import Data.Text (Text)
 import qualified Types.API.User as API (CreateUserRequest (..))
-import qualified Types.DB.User as DB (User)
+import qualified Types.DB.User as DB (User (..))
 
 data RoutingResult = UnknownMethod | UnknownEndpoint | RoutingSuccess deriving (Show, Eq)
 
@@ -12,7 +13,7 @@ data RoutingHandle m req resp = MainHandle
     hUnknownMethodResp :: resp,
     hUnknownEndpointResp :: resp,
     hAuth :: req -> m DB.User,
-    createUserHandler :: CreateUserHandle m req
+    createUserHandler :: CreateUserHandle m req resp
   }
 
 hRouting :: Monad m => RoutingHandle m req resp -> req -> m (RoutingResult, resp)
@@ -33,11 +34,27 @@ hRouting MainHandle {..} request = do
 
 data CreateUserResult = IncorrectRequest | NotAdmin | LoginIsTaken | CreateUserSuccess deriving (Show, Eq)
 
-data CreateUserHandle m req = CreateUserHandle
-  { hParseRequest :: req -> API.CreateUserRequest,
+data CreateUserHandle m req resp = CreateUserHandle
+  { hParseRequest :: req -> Maybe API.CreateUserRequest,
     hFindUserByLogin :: Text -> m (Maybe Int),
-    hWriteUser :: API.CreateUserRequest -> m ()
+    hWriteUser :: API.CreateUserRequest -> m (),
+    hIncorrectRequestResp :: resp,
+    hNotAdminResp :: resp,
+    hLoginIsTakenResp :: resp,
+    hCreateUserSuccessResp :: resp
   }
 
-hCreateUser :: CreateUserHandle m req -> DB.User -> req -> m (CreateUserResult, resp)
-hCreateUser = undefined
+hCreateUser :: Monad m => CreateUserHandle m req resp -> DB.User -> req -> m (CreateUserResult, resp)
+hCreateUser CreateUserHandle {..} DB.User {..} r = do
+  case hParseRequest r of
+    Nothing -> pure (IncorrectRequest, hIncorrectRequestResp)
+    Just req@API.CreateUserRequest {..} ->
+      if not isAdmin
+        then pure (NotAdmin, hNotAdminResp)
+        else do
+          mbUserId <- hFindUserByLogin reqLogin
+          if isJust mbUserId
+            then pure (LoginIsTaken, hLoginIsTakenResp)
+            else do
+              hWriteUser req
+              pure (CreateUserSuccess, hCreateUserSuccessResp)
